13c40c81bdbfc3e775819afb322f76185f79b4e0
XEROINSIDE\raquel.nishimoto <raquel.nishimoto@xero.com>
2021-08-21 16:15:49 +1200

    Add googlemap project

---
+++ src/CustomMap.ts
@@ -0,0 +1,40 @@
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number
+  };
+  makeMarkerContent(): string;
+  color: string;
+}
+
+export class CustomMap {
+  googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      }
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.makeMarkerContent(),
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
